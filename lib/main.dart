6b765a3bb9335cf05cd3428cd01cb1696254535c
KARTHICK SAKTHIVEL <54999654+karthick794@users.noreply.github.com>
2024-07-26 23:40:02 +0530

    device lock code

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/screens/quiz_screen.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(const QuizScreen());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])  //lock orientation
+      .then((fn) {
+    runApp(const QuizScreen());
+  });
 }
-
-
- 
